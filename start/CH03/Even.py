147045774c2fc9de2ee9e1234628b19e3846e2df
JamesMalcolm-Greenriver <Malcolm.James@student.greenriver.edu>
2024-10-03 07:22:58 -0700

    Added and completed Even.py

---
+++ start/CH03/Even.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Is the given number even?
+#By James Malcolm
+num = int(input("Please provide a number: "))
+
+if num % 2 == 0:
+    print(str(num) + " is even.")
+else:
+    print(str(num) + " is NOT even.")
