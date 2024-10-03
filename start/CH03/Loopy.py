f842b485779d9e2f83933ce2dc38ad0e0a9f26df
JamesMalcolm-Greenriver <Malcolm.James@student.greenriver.edu>
2024-10-03 07:17:11 -0700

    Completed Loopy.py

--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
 # example workign with Loops
-#By 
+#By James Malcolm
+answer = input("Is today a good day? (y/n)")
+
+if answer == 'y':
+    for i in range(0,10):
+        print("Yes it is")
