d58f7d13a7f5c13e5b8b6591edcc4b46f44ef1e5
JamesMalcolm-Greenriver <Malcolm.James@student.greenriver.edu>
2024-10-20 06:08:46 -0700

    Implemented function send_message

--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 # example workign with conditionals
-#By James Malcolm
-answer = input("Is today a good day? (y/n)")
+# By James Malcolm based on class lecture video
+# Date: 10/18/24
+    
+def send_message():
+    while True:
+        ans = input("Is today a great day? (y/n): ".lower())
+        if ans in ['y', 'n']:
+            return ans
+        else:
+            print("Not a valid reponse, please input y or n")
+            
+ans = send_message()
 
-if answer == 'y':
-    print("Yes it is")
+if ans == 'y':
+    for i in range(10):
+        print("Yes it is.")
+else:
+    print("Today is not the best day.")
