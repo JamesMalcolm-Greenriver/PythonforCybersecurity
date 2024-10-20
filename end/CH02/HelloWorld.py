0e456799d7d4601526fdde2dc2ff1a8f1b6bfcce
JamesMalcolm-Greenriver <Malcolm.James@student.greenriver.edu>
2024-10-20 05:43:33 -0700

    temp

--- end/CH02/HelloWorld.py
+++ end/CH02/HelloWorld.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python
-# Created by Ed Goad, 2/3/2021
-print("Hello World")
+# Created by James Malcolm, 09/23/2024
+
+# 
+# print(f"Hello {input("Q: What is your name?\n")}. In 2 years you will be {int(input("Q: And how old are you?\n"))+ 2} years old! Today is going to be a great day!")
+
+name = input("Please enter your name: ")
+
+print(f"Hello {name}.\nToday will be a great day {name}!")
+
+age = int(input("Please enter your age: "))
+
+future_age = age + 2
+
+print(f"In 2 years you will be {future_age} years old.")
--- end/CH04/WriteFile.py
+++ end/CH04/WriteFile.py
@@ -4,13 +4,15 @@
 # 2/27/2021
 
 # Open file for writing
-test_file = open("testfile.txt", "w")
+# test_file = open("testfile.txt", "w")
 
-# Write lines to the file
-test_file.write("Hello World\n")
-test_file.write("My name is Ed\n")
-test_file.write("I like rubber ducks\n")
+# # Write lines to the file
+# test_file.write("Hello World\n")
+# test_file.write("My name is Ed\n")
+# test_file.write("I like rubber ducks\n")
 
-# Close the file
-test_file.close()
+# # Close the file
+# test_file.close()
+
+
 
