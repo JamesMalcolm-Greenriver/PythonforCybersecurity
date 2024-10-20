8d5c51f269452c1a4f6df14317b9470f77eeb575
JamesMalcolm-Greenriver <Malcolm.James@student.greenriver.edu>
2024-10-20 06:09:34 -0700

    Changed formatting for text file

--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -4,10 +4,10 @@
 
 hack_file = open("hackme.txt", "w")
 
-hack_file.write(input("What is your name? ") + "\n")
-hack_file.write(input("What is your favorite color? ") + "\n")
-hack_file.write(input("What was your first pet's name? ") + "\n")
-hack_file.write(input("What is your mother's maiden name? ") + "\n")
-hack_file.write(input("What elementary school did you attend? ") + "\n")
+hack_file.write("Name: " + input("What is your name? ") + "\n")
+hack_file.write("Favorite Color: " + input("What is your favorite color? ") + "\n")
+hack_file.write("First Pet's Name: " + input("What was your first pet's name? ") + "\n")
+hack_file.write("Mother's Maiden Name: " + input("What is your mother's maiden name? ") + "\n")
+hack_file.write("Elementary School: " + input("What elementary school did you attend? ") + "\n")
 
 hack_file.close()
