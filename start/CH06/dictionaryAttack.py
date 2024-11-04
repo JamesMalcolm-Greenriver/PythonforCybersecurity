d2c08411368e38ad644bfd565158d75f0a6c2e5a
JamesMalcolm-Greenriver <Malcolm.James@student.greenriver.edu>
2024-11-03 20:50:37 -0800

    Password cracking script

--- start/CH06/dictionaryAttack.py
+++ start/CH06/dictionaryAttack.py
@@ -1,4 +1,43 @@
 #!/usr/bin/env python3
 # Script that performs a dictionary attack against known password hashes
 # Needs a dictionary file, suggested to use https://github.com/danielmiessler/SecLists/tree/master/Passwords/Common-Credentials
-# By 
+# By James Malcolm
+# Date: 11/2/2024
+
+from passlib.hash import sha512_crypt
+
+ShadowFile = "C:/School/Fall2024/IT102 - Scripting/Python/Repo/PythonforCybersecurity/start/CH06/shadow"
+
+PasswordFile = "C:/School/Fall2024/IT102 - Scripting/Python/Repo/PythonforCybersecurity/start/CH06/top10000.txt"
+
+def test_password():
+    return False
+    
+    
+def guess_password(ShadowFile, PasswordFile):
+    correct = []
+    
+    with open(ShadowFile, 'r') as sfile, open(PasswordFile, 'r') as pfile:
+        shadows = sfile.readlines()
+        passwords = pfile.readlines()
+        
+        for shadow in shadows:
+            con = shadow.split(':')
+            if len(con) < 2 or '!' in con[1] or '*' in con[1]:
+                continue
+            
+            user, hpass = con[0], con[1].strip()
+            
+            for password in passwords:
+                password = password.strip()
+                
+                print(f"Trying password for {user}: {password}")
+
+                try:
+                    if sha512_crypt.verify(password, hpass):
+                        correct.append((user, hpass))
+                        print(f"Found, {user}: {password}")
+                except ValueError:
+                    continue
+
+guess_password(ShadowFile, PasswordFile)
